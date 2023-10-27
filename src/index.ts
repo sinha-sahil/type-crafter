ac522fa560db9e24ef7278d80f175f888ee55776
Sahil Sinha <sahilsinha.dar@gmail.com>
2023-10-27 18:56:59 +0530

    feat: added generic generator for data read from spec
    - added handlebars for template interpretation & compilation
    - added generic generator for generating types for objects
    - added basic object syntax template for typescript
    - updated generator function input to provide complete configuration
    - updated examples to meet new generator input

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import yaml from "yaml";
 import { Configuration, decodeSpecFileData } from "$types";
 import { readFile } from "$utils";
+import { generator } from "$generators/generic";
 
-export async function generate(configuration: Configuration): Promise<void> {
+export async function generate(config: Configuration): Promise<void> {
   try {
-    const specFileData = await readFile(configuration.inputFile);
+    const specFileData = await readFile(config.input);
     const specJSONData = yaml.parse(specFileData);
     const decodedSpecData = decodeSpecFileData(specJSONData);
     if (decodedSpecData === null) {
       throw new Error("Invalid spec file data!");
     }
+    generator(config, decodedSpecData);
   } catch (e) {
     console.error("Generation failed!");
   }
